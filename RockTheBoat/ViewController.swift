93682643a0413825210f1ea5fe8e8f4c72c9fe0b
Wayne <jxu51@u.rochester.edu>
2019-10-08 23:30:06 -0400

    1

--- RockTheBoat/ViewController.swift
+++ RockTheBoat/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func imageFadein(){
+        UIImageView.animate(withDuration: 1) {
+            self.image.alpha = 100
+        }
+    }
+    
+    func constraint1(_ show: Bool) {
+        let newYPosition = show ? self.view.frame.height : -self.view.frame.height
+        let delay = show ? 0.5 : 0.0
+        UIView.animate(withDuration: 1.2, delay: delay, options: [.curveEaseInOut], animations: { self.stackViewBottomConstraint.constant += newYPosition
+            self.view.layoutIfNeeded()
+        })
+        
+    }
+    
+    func rotate(){
+        let angle =  CGFloat(Double.pi/2)
+        let tr = CGAffineTransform.identity.rotated(by: angle)
+        
+        UIImageView.animate(withDuration: 0.7, animations: {
+            self.image.transform = tr
+        }, completion: { _ in
+            UIImageView.animate(withDuration: 0.5, animations: {
+                self.image.transform = CGAffineTransform.identity.rotated(by: angle*2)
+            }, completion: { _ in
+                UIImageView.animate(withDuration: 0.5, animations: {
+                    self.image.transform = CGAffineTransform.identity.rotated(by: angle*4)
+                })
+            })
+        })
+    }
+    
+    @IBAction func alphaChange(_ sender: Any) {
+        imageFadein()
+    }
+    
+    
+    @IBAction func stackAnim(_ sender: Any) {
+        
+    }
+    
+    
 }
 
